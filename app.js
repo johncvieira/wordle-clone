fcc7825e2659703f238a6f025b3d175ee74e4d13
John Vieira <john.c.vieira@gmail.com>
2022-02-09 12:11:22 -0500

    Add gitignore and small app.js fix

--- app.js
+++ app.js
@@ -1,71 +1,40 @@
-let keys = {'q' : '', 'w': '', 'e': '', 'r': '', 't': '', 'y': '', 'u': '', 'i': '', 'o': '', 'p': '', 'break': '',
-              'a': '', 's': '', 'd': '', 'f': '', 'g': '', 'h': '','j': '','k': '', 'l': '', 'break2': '',
-            'enter': '', 'z':'', 'x': '', 'c': '', 'v': '', 'b': '', 'n': '', 'm': '', '⌫': ''};
+let keys = {
+  'q': '', 'w': '', 'e': '', 'r': '', 't': '', 'y': '', 'u': '', 'i': '', 'o': '', 'p': '', 'break': '',
+  'a': '', 's': '', 'd': '', 'f': '', 'g': '', 'h': '', 'j': '', 'k': '', 'l': '', 'break2': '',
+  'enter': '', 'z': '', 'x': '', 'c': '', 'v': '', 'b': '', 'n': '', 'm': '', '⌫': ''
+};
 
 let guesses = [];
 let currentGuess = [];
 
 const SecretWord = 'jcode';
-const NumberOfGuesses = 5;
+const NumberOfGuesses = 6;
 const Correct = 'correct';
 const Found = 'found';
 const Wrong = 'wrong';
 
-function initialize(){
+function initialize() {
   let guessGrid = document.getElementById("guessGrid");
-  for(let i=0; i < NumberOfGuesses;i++){
-    for(let j=0; j < SecretWord.length; j++){
+  for (let i = 0; i < NumberOfGuesses; i++) {
+    for (let j = 0; j < SecretWord.length; j++) {
       guessGrid.innerHTML += `<div id="${i}${j}" class="key-guess"></div>`
     }
     guessGrid.innerHTML += '<br/>'
   }
-  
+
   let keyboard = document.getElementById("keyboard");
   Object.keys(keys).forEach((key) => {
-    if(key.includes('break')){
+    if (key.includes('break')) {
       keyboard.innerHTML += '<br/>';
-    }else{
+    } else {
       keyboard.innerHTML += `<button id="${key}" class="key" onclick="keyClick('${key}')">` + key + '</button>';
     }
   });
 }
 initialize()
 
-function enter(){
-  console.log('guesses.length', guesses.length);
-  if(currentGuess.length < SecretWord.length || guesses.length >= NumberOfGuesses){
-    return;
-  }
-  console.log('enter');
-  //TODO check a dictionary for a valid word
-
-  currentGuess.forEach((keyGuess, index) => {
-    if(SecretWord.charAt(index) == keyGuess.key){
-      keyGuess.result = Correct
-    }else if (SecretWord.includes(keyGuess.key)){
-      keyGuess.result = Found
-    }else {
-      keyGuess.result = Wrong
-    }
-
-    if(keys[keyGuess.key] != Correct){
-      keys[keyGuess.key] = keyGuess.result 
-    }
-  });
-  updateCurrentGuess(true);
-  guesses.push(currentGuess);
-  currentGuess = [];
-}
-
-function backspace(){
-  if(currentGuess.length > 0){
-    currentGuess.pop();
-  }
-  updateCurrentGuess();
-}
-
-function keyClick(key){
-  switch(key) {
+function keyClick(key) {
+  switch (key) {
     case '⌫':
       backspace();
       break;
@@ -73,18 +42,49 @@ function keyClick(key){
       enter();
       break;
     default:
-      if(currentGuess.length < SecretWord.length 
-        && guesses.length < NumberOfGuesses){
-        currentGuess.push({key: key, result: ''});
+      if (currentGuess.length < SecretWord.length
+        && guesses.length < NumberOfGuesses) {
+        currentGuess.push({ key: key, result: '' });
         updateCurrentGuess();
       }
   }
 }
 
-function updateKeyboard(){
+function backspace() {
+  if (currentGuess.length > 0) {
+    currentGuess.pop();
+  }
+  updateCurrentGuess();
+}
+
+function enter() {
+  if (currentGuess.length < SecretWord.length || guesses.length >= NumberOfGuesses) {
+    return;
+  }
+  //TODO check a dictionary for a valid word
+
+  currentGuess.forEach((keyGuess, index) => {
+    if (SecretWord.charAt(index) == keyGuess.key) {
+      keyGuess.result = Correct
+    } else if (SecretWord.includes(keyGuess.key)) {
+      keyGuess.result = Found
+    } else {
+      keyGuess.result = Wrong
+    }
+
+    if (keys[keyGuess.key] != Correct) {
+      keys[keyGuess.key] = keyGuess.result
+    }
+  });
+  updateCurrentGuess(true);
+  guesses.push(currentGuess);
+  currentGuess = [];
+}
+
+function updateKeyboard() {
   for (const key in keys) {
-    let keyElement = document.getElementById(`${key}`);
-    if(keys[key] != ''){
+    if (keys[key] != '') {
+      let keyElement = document.getElementById(`${key}`);
       keyElement.className = '';
       keyElement.classList.add(keys[key]);
       keyElement.classList.add('key');
@@ -92,20 +92,20 @@ function updateKeyboard(){
   }
 }
 
-function updateCurrentGuess(guessed = false){
+function updateCurrentGuess(guessed = false) {
   let index = guesses.length;
-  for(let i=0; i<SecretWord.length; i++){
+  for (let i = 0; i < SecretWord.length; i++) {
     let guessGrid = document.getElementById(`${index}${i}`);
-    if(currentGuess[i]){
+    if (currentGuess[i]) {
       guessGrid.innerHTML = currentGuess[i].key;
-    }else{
+    } else {
       guessGrid.innerHTML = '';
     }
-    if(guessed){
+    if (guessed) {
       guessGrid.classList.add(currentGuess[i].result);
     }
   }
-  if(guessed){
+  if (guessed) {
     updateKeyboard();
   }
 }
